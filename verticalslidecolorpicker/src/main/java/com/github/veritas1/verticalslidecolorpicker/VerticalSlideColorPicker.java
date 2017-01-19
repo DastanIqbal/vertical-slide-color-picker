8441bacc443f9ae244321921ee52c444f1d945ce
Nuno Luis <nuno.lopes.luis@coins-global.com>
2017-01-19 11:28:42 +0000

    Draw the selection line with a contrasting grayscale

--- verticalslidecolorpicker/src/main/java/com/github/veritas1/verticalslidecolorpicker/VerticalSlideColorPicker.java
+++ verticalslidecolorpicker/src/main/java/com/github/veritas1/verticalslidecolorpicker/VerticalSlideColorPicker.java
@@ -22,6 +22,7 @@ public class VerticalSlideColorPicker extends View {
 
 	private Paint paint;
 	private Paint strokePaint;
+	private Paint selectorStrokePaint;
 	private Path path;
 	private Bitmap bitmap;
 	private int viewWidth;
@@ -83,8 +84,9 @@ public class VerticalSlideColorPicker extends View {
 		strokePaint.setAntiAlias(true);
 		strokePaint.setStrokeWidth(borderWidth);
 
-		setDrawingCacheEnabled(true);
+		selectorStrokePaint = new Paint(strokePaint);
 
+		setDrawingCacheEnabled(true);
 	}
 
 	@Override
@@ -99,7 +101,7 @@ public class VerticalSlideColorPicker extends View {
 			cacheBitmap = false;
 			invalidate();
 		} else {
-			canvas.drawLine(colorPickerBody.left, selectorYPos, colorPickerBody.right, selectorYPos, strokePaint);
+			canvas.drawLine(colorPickerBody.left + borderWidth/2, selectorYPos, colorPickerBody.right - borderWidth/2, selectorYPos, selectorStrokePaint);
 		}
 	}
 
@@ -111,6 +113,9 @@ public class VerticalSlideColorPicker extends View {
 
 		selectorYPos = yPos;
 		int selectedColor = bitmap.getPixel(viewWidth/2, (int) selectorYPos);
+
+		int scaleValue = 255 - (int) (calculateLuminance(selectedColor) * 255);
+		selectorStrokePaint.setColor(Color.rgb(scaleValue, scaleValue, scaleValue));
 
 		if (onColorChangeListener != null) {
 			onColorChangeListener.onColorChange(selectedColor);
@@ -182,6 +187,20 @@ public class VerticalSlideColorPicker extends View {
 		this.colors = colors;
 		cacheBitmap = true;
 		invalidate();
+	}
+
+	// from android.support.v4.graphics.ColorUtils#calculateLuminance
+	private static double calculateLuminance(int color) {
+		double red = Color.red(color) / 255d;
+		red = red < 0.03928 ? red / 12.92 : Math.pow((red + 0.055) / 1.055, 2.4);
+
+		double green = Color.green(color) / 255d;
+		green = green < 0.03928 ? green / 12.92 : Math.pow((green + 0.055) / 1.055, 2.4);
+
+		double blue = Color.blue(color) / 255d;
+		blue = blue < 0.03928 ? blue / 12.92 : Math.pow((blue + 0.055) / 1.055, 2.4);
+
+		return (0.2126 * red) + (0.7152 * green) + (0.0722 * blue);
 	}
 
 	public void resetToDefault() {
